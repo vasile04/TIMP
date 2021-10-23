53b81305582228b9c054b0d818173b74b2b910d9
vasile04 <70753493+vasile04@users.noreply.github.com>
2021-10-23 20:40:31 +0300

    Add files via upload

--- maculator#2.py
+++ maculator#2.py
@@ -1,13 +1,28 @@
-import datetime
+import numpy as np
+z=np.arange(3, 15, 1)
+print(z)
+n= int(input('Introdu numarul: '))
+for i in range(1, n) :
+    {print(i, end=',  ')}
+
+
 import time
+while True:
+    localtime = time.localtime()
+    result = time.strftime("%H:%M:%S %p", localtime)
+    print(result, end="")
+    print("\r", end="")
+    time.sleep(1)
+
+import datetime
 from datetime import timedelta
 z = datetime.datetime.now()
-ma= z - timedelta(100)
-if z - ma==timedelta(100):
+ma= z - timedelta(abs(int(input('Introdu durata/nr de zile: '))))
+if z - ma>=timedelta(abs(100)):
     print('true')
 else:
     print('false')
-print(datetime.datetime.strftime(ma, "%d-%B-%Y"))
+print(datetime.datetime.strftime(ma, "%A-%d-%B-%Y"))
 w=datetime.datetime.now()
 q=w.strftime( '%H : %M : %S  %p') 
 print(q)
